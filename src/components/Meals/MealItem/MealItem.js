0c220b62d5bbc17f1b1f5155d86b1b1fc2f1171b
PraveenNarwal <praveennarwal5@gmail.com>
2021-07-03 23:28:54 +0530

    mealItem map

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,22 @@
+import { classes } from 'istanbul-lib-coverage';
+import React from 'react'
+import clasess from './MealItem.module.css'
+
+
+function MealItem(props) {
+    const price = `$${props.price.toFixed(2)}`;
+    return (
+        <li className={classes.meal}>
+            <div>
+                <h3>{props.name}</h3>
+                <div className={classes.description}>{props.description}</div>
+                <div className={classes.price}>{props.price}</div>
+            </div>
+            <div>
+
+            </div>
+        </li>
+    )
+}
+
+export default MealItem
