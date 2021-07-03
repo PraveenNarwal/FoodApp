98fcb18afe52a61e5c7d9740f000e5ba157b3322
PraveenNarwal <praveennarwal5@gmail.com>
2021-07-04 00:51:13 +0530

    cart componnet

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import classes from './Cart.module.css'
+import Modal from '../UI/Modal'
+
+
+function Cart() {
+    const CartItems = <ul className={classes['cart-items']}>{[{
+        id: 'c1',
+        name: 'Sushi',
+        amount: 2,
+        price: 12.99
+    }].map(items => <li>{items.name}</li>)}</ul>
+
+    return (
+        <Modal>
+            {CartItems}
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>35.55</span>
+            </div>
+            <div className={classes.actions}>
+                <button className={classes['button--alt']}>Close</button>
+                <button className={classes.button}>Order</button>
+            </div>
+
+        </Modal>
+    )
+}
+
+export default Cart
